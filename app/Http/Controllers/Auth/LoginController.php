3e8a512d1fae2b9d20c1771727f252e2ce530a43
root <root@vagrant.vm>
2017-05-25 08:02:32 +0000

    cute

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -39,6 +39,11 @@ class LoginController extends Controller
         $this->middleware('guest', ['except' => 'logout']);
     }
     
+//    public function showLoginForm()
+//    {
+//    	return redirect()->guest('/contribute/login');
+//    }
+    
     
     public function logout()
     {
