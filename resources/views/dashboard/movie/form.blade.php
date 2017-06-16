da5e01cda5da26c1547e8af4609f70c09044f2fc
root <root@vagrant.vm>
2017-06-16 09:13:37 +0000

    cute

--- resources/views/dashboard/movie/form.blade.php
+++ resources/views/dashboard/movie/form.blade.php
@@ -199,6 +199,7 @@
 
             @endif
 
+
 			@if(Ctm::isDev())
 			<div class="form-group">
                 <div class="col-md-4 col-md-offset-3">
@@ -209,6 +210,7 @@
 
         </form>
 
+		aaaaa
 		<div class="col-md-12 text-right">
             <a href="{{ url('dashboard/model-movies/' . $mvCombine->rel_id) }}" class="btn btn-warning center-block">この動画を結合し直す >></a>
         </div>
