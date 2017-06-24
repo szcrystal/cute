fbd255fc938f85d29ce33e7005344f35c5fe4c8c
szcrystal <szk.create@gmail.com>
2017-06-24 05:22:37 +0000

    cute

--- app/Http/Controllers/Auth/ResetPasswordController.php
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -25,7 +25,7 @@ class ResetPasswordController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/contribute'; //Reset後は自動ログインされる
 
     /**
      * Create a new controller instance.
