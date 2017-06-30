92815d6838f5a0758f999c03617d8e46c6dc2431
szcrystal <szk.create@gmail.com>
2017-06-30 07:48:41 +0000

    cute

--- resources/views/main/model/single.blade.php
+++ resources/views/main/model/single.blade.php
@@ -123,6 +123,20 @@
                 </div>
             </div><!-- panelbody -->
 
+            <div class="my-3 col-md-12">
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- cute.campus2017 -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-8915257973665991"
+     data-ad-slot="7606073469"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</div>
+
         </div>
+
 
 @endsection
